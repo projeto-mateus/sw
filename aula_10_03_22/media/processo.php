c7293c3bfce4fd71636ce1b269d69b246ce8e001
Mateus Alves Moraes <80174933+projeto-mateus@users.noreply.github.com>
2022-04-01 21:42:52 -0300

    Add files via upload

---
+++ aula_10_03_22/media/processo.php
@@ -0,0 +1,10 @@
+<?php
+    $num1= $_REQUEST['num1'];
+    $num2= $_REQUEST['num2'];
+    $num3= $_REQUEST['num3'];
+
+    $soma = ($num1 + $num2 + $num3) /3 ;
+
+    echo" <p>A media é : $soma</p>";
+
+?>
